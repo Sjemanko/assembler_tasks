b247031538e6be371936579ea338d1b98eb1c7fd
Tomasz Bułecki <96573032+Sjemanko@users.noreply.github.com>
2024-11-19 11:05:06 +0100

    Add files via upload

--- tasks/ascii.asm
+++ tasks/ascii.asm
@@ -3,6 +3,21 @@
 	string: .asciiz "000123897"
 .text
 
-	la $a0, string
+	la $a0, string			# $a0 = &string
+	
+	
+	# pêtla for po string
+	
+		# element = load byte 
+		
+		# if element == digit (ascii - 48 >= 0 && ascii - 48 <= 9)
+			# save digit to new word (?)
+			# element += 1
+			
+		# else if (ascii === 000) 
+			# end for, return 0
+		
+		# else 	
+			# return -1
 	
 	
